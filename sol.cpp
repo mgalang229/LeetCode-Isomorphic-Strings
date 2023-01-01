77248e26ca8f1a785ca95f39e9efef420c31d320
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2023-01-01 23:45:17 +0800

    Create sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        // b -> b and d -> b
+        // c -> j and c -> h
+        map<char, char> mp1, mp2;
+        for(int i = 0; i < (int) s.size(); i++) {
+            if(mp1[s[i]] && mp1[s[i]] != t[i]) {
+                return false;
+            }
+            if(mp2[t[i]] && mp2[t[i]] != s[i]) {
+                return false;
+            }
+            mp1[s[i]] = t[i];
+            mp2[t[i]] = s[i];
+        }
+        return true;
+    }
+};
